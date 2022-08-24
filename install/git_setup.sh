531dbda5f467d58a29e658bd76fa12bb67c9ba65
Brad Holmes <inkspotch@gmail.com>
2022-08-24 14:55:18 -0500

    Fix git to pull.rebase

--- install/git_setup.sh
+++ install/git_setup.sh
@@ -3,4 +3,5 @@ git config --global alias.co checkout
 git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.sw switch
-git config --global push.rebase true
+git config --global pull.rebase true
+git config --global push.default current
